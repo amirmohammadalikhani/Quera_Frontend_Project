95cac9800c3c97eda7d693fe0dfa0994f0960883
Amir Alikhani <alikhaniamir83@gmail.com>
2024-05-22 20:43:33 +0330

    the indention of layout root just fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 		<html lang="en" dir="ltr">
 			<body className={inter.className}>
 				<Header></Header>
-                {children}
+				{children}
 			</body>
 		</html>
 	);
